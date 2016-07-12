afa2729899fd0b60813f38615c7fab9de07cad19
Andrei Bosco <andreibosco@virtual.ufc.br>
2016-07-11 22:11:46 -0300

    adicionando plot dos dias de captura de dados

--- matlab/sglog.m
+++ matlab/sglog.m
@@ -8,8 +8,33 @@ load('sglog-7a13');
 %     dia(i) = SGLOG(filtro, :);
 % end
 
-filtro = SGLOG.dia == 9;
+figure;
+hold on;
+grid on;
+grid minor;
+filtro = SGLOG.dia == 8;
 teste = SGLOG(filtro, :);
-
 % plot(SGLOG.sensorValor);
 plot(teste.sensorValor);
+
+filtro = SGLOG.dia == 9;
+teste = SGLOG(filtro, :);
+plot(teste.sensorValor);
+
+filtro = SGLOG.dia == 10;
+teste = SGLOG(filtro, :);
+plot(teste.sensorValor);
+
+filtro = SGLOG.dia == 11;
+teste = SGLOG(filtro, :);
+plot(teste.sensorValor);
+
+filtro = SGLOG.dia == 12;
+teste = SGLOG(filtro, :);
+plot(teste.sensorValor);
+
+filtro = SGLOG.dia == 13;
+teste = SGLOG(filtro, :);
+plot(teste.sensorValor);
+
+legend('dia 8','dia 9','dia 10','dia 11','dia 12','dia 13');
