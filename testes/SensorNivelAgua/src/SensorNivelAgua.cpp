de4d1739427ec3e21d24988962e6034297609d66
andreibosco <andreibosco@gmail.com>
2016-09-08 23:50:22 -0300

    adicionando exemplo basico de uso do sensor de nível de agua

---
+++ testes/SensorNivelAgua/src/SensorNivelAgua.cpp
@@ -0,0 +1,18 @@
+// baseado de http://www.arduinoecia.com.br/2014/07/arduino-sensor-de-nivel-de-liquidos.html
+
+#include <Arduino.h>
+
+int pinoSensor = 3; // pino digital ligado ao sensor
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinoSensor, INPUT);
+}
+
+void loop() {
+  int estado = digitalRead(pinoSensor);
+  Serial.print("Estado do Sensor: ");
+  Serial.println(estado);
+
+  delay(1000); // 1s
+}
