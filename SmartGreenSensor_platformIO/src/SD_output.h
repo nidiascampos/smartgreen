5c0ddaa2e25e0cbd550d4ca0f3c8a8222bec5da5
Andrei Bosco <andreibosco@virtual.ufc.br>
2016-10-01 18:42:33 -0300

    smartgreen: alterando pino do modulo SD

--- SmartGreenSensor_platformIO/src/SD_output.h
+++ SmartGreenSensor_platformIO/src/SD_output.h
@@ -10,4 +10,4 @@
 // used as the CS pin, the hardware CS pin (10 on most Arduino boards,
 // 53 on the Mega) must be left as an output or the SD library
 // functions will not work.
-const int chipSelect = 4;
+const int chipSelect = 10;
