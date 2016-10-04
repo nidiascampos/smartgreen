d38bb368447d7c43065c1566f0b74cd5329ceddf
Andrei Bosco <andreibosco@virtual.ufc.br>
2016-10-03 23:10:37 -0300

    smartgreen3wm: começando a incluir código de envio de dados pela rede celular

--- SmartGreenSensor_3WM_platformIO/src/main.cpp
+++ SmartGreenSensor_3WM_platformIO/src/main.cpp
@@ -4,17 +4,9 @@
 #include <math.h> // Conversion equation from resistance to %
 #include <SPI.h>
 /*
-WM01
-D4 -> A0
-D5 -> A1
-
-WM02
-D6 -> A2
-D7 -> A3
-
-WM03
-D8 -> A6
-D9 -> A7
+WM01      | WM02      | WM03
+D4 -> A0  | D6 -> A2  | D8 -> A6
+D5 -> A1  | D7 -> A3  | D9 -> A7
 */
 
 // SD output
@@ -29,6 +21,7 @@ D9 -> A7
 #include "RTC.h"
 #include "SD_output.h"
 #include "Watermark.h"
+#include "SIM900.h"
 
 // Formato do output (CSV)
 // ano, mês, dia, hora, minuto, segundo, temperatura,
@@ -38,11 +31,6 @@ void setup ()
 {
   Serial.begin(57600);
   Serial.println("DEBUG: Iniciando...");
-
-  // DEBUG: para informar quando o sketch foi compilado
-  // Serial.print("compiled: ");
-  // Serial.print(__DATE__);
-  // Serial.println(__TIME__);
 
   // initialize the digital pins as an output.
   // Pin 4,5 is for sensor 1
@@ -105,7 +93,6 @@ void setup ()
   Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
 
   // see if the card is present and can be initialized:
-
   Serial.print("DEBUG:  SD -> ");
   if (!SD.begin(chipSelect)) {
     Serial.println("Cartao SD falhou ou nao esta presente");
@@ -114,6 +101,10 @@ void setup ()
   }
   Serial.println("Cartao SD inicializado");
 
+  // SIM900 modem
+  // powerOnSIM900();
+  // Serial.println("Conectando à rede da operadora");
+  // while( sendATcommand2("AT+CREG?", "+CREG: 0,1", "+CREG: 0,5", 1000)== 0 );
 }
 
 void loop ()
@@ -138,14 +129,12 @@ void loop ()
   long read2= average();
   long sensor1 = (read1 + read2)/2;
 
-  // measure: sensor id, phase B pin, phase A pin, analog input pin
   measure(2,6,7,3);
   long read3 = average();
   measure(2,7,6,2);
   long read4= average();
   long sensor2 = (read3 + read4)/2;
 
-  // measure: sensor id, phase B pin, phase A pin, analog input pin
   measure(3,8,9,7);
   long read5 = average();
   measure(3,9,8,6);
@@ -168,15 +157,23 @@ void loop ()
   dataString += ",";
   dataString += String(sensor3); // sensor bias compensated value
 
+  // SIM900
+  long sensorsData[3] = {sensor1, sensor2, sensor3};
+  Serial.print("sensors data: ");
+  Serial.println(sensorsData[0]);
+  Serial.println(sensorsData[1]);
+  Serial.println(sensorsData[2]);
+  // sendDataToCloud(sensorsData);
+  // powerDownSIM900();
+
   // DEBUG: print to the serial port
   Serial.print(dataString);
   Serial.println();
 
+  // SD
   // open the file:
   File dataFile = SD.open("SGlog.csv", FILE_WRITE);
-
   // if the file is available, write to it:
-
   if (dataFile) {
     dataFile.println(dataString);
     dataFile.close();
