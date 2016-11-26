305d25aae1d1ba791c0bda552971924ff968eb4b
Andrei Bosco <andreibosco@gmail.com>
2016-11-25 23:51:01 -0300

    testes python: melhorando organização do código;

--- testes/python/mongodb-mqtt01.py
+++ testes/python/mongodb-mqtt01.py
@@ -1,33 +1,36 @@
 import paho.mqtt.client as mqtt
 
+
 def get_db():
-  from pymongo import MongoClient
-  client = MongoClient('localhost:27017')
-  db = client.SmartGreen
-  return db
+    from pymongo import MongoClient
+    client_mongo = MongoClient('localhost:27017')
+    db = client_mongo.SmartGreen
+    return db
+
 
 def mqtt_connect(client, userdata, rc):
-  print("Conectado com resultado "+str(rc))
-  client.subscribe("/sensor/#")
+    print("Connected with result: "+str(rc))
+    client.subscribe("/sensor/#")
 
 # def add_message(db):
-#   db.teste03.insert({"name": "Canada"})
+#     db.teste03.insert({"name": "Canada"})
+
 
 def mqtt_message(client, userdata, msg):
-  # print(msg.topic+" "+str(msg.payload))
-  print("Received message '" + str(message.payload) + "' on topic '"
-        + message.topic + "' with QoS " + str(message.qos))
-  print("User data " + str(userdata));
-  print("Client " + client.client_id);
+    # print(msg.topic+" "+str(msg.payload))
+    print("Received message '" + str(msg.payload) + "' on topic '"
+          + msg.topic + "' with QoS " + str(msg.qos))
+    print("User data " + str(userdata))
+    print("Client " + client.client_id)
 
-  # db.teste03.insert({
-  # 	_id: 
-  # 	})
+    # db.teste03.insert({
+    # 	_id:
+    # 	})
 
-client = mqtt.Client()
-client.on_connect = mqtt_connect
-client.on_message = mqtt_message
+client_mqtt = mqtt.Client()
+client_mqtt.on_connect = mqtt_connect
+client_mqtt.on_message = mqtt_message
 
-client.connect("localhost", 1883, 60)
+client_mqtt.connect("localhost", 1883, 60)
 
-client.loop_forever()
+client_mqtt.loop_forever()
