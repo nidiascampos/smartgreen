5244c58d8c8ae6bf671b921af1afc6000c551de8
Andrei Bosco <andreibosco@gmail.com>
2016-11-25 23:49:10 -0300

    testes python: melhorando organização do código;

--- testes/python/mqtt02.py
+++ testes/python/mqtt02.py
@@ -1,8 +1,11 @@
 import paho.mqtt.client as mqtt
+from pymongo import MongoClient
+
 
 def mqtt_connect(client, userdata, rc):
-    print("Conectado com resultado "+str(rc))
+    print("Connected with result: "+str(rc))
     client.subscribe("/sensor/#")
+
 
 def mqtt_message(client, userdata, msg):
     # print(msg.topic+" "+str(msg.payload))
@@ -11,28 +14,28 @@ def mqtt_message(client, userdata, msg):
     print("User data " + str(userdata))
     mongo_add_message(msg)
 
+
 def mongo_add_message(msg):
-    from datetime import datetime
-    date = datetime.utcnow()
+    import datetime
     db.teste03.update_one({
         "_id": msg.topic
-    },{
+    }, {
         "$push": {
             "events": {
                 "value": msg.payload,
-                "when": date
+                "when": datetime.datetime.utcnow()
             }
         }
-    }, upsert= True)
+    }, upsert=True)
 
-from pymongo import MongoClient
+
+# DB
 clientMongo = MongoClient('localhost:27017')
 db = clientMongo.SmartGreen
 
-client = mqtt.Client()
-client.on_connect = mqtt_connect
-client.on_message = mqtt_message
-
-client.connect("localhost", 1883, 60)
-
-client.loop_forever()
+# MQTT
+client_mqtt = mqtt.Client()
+client_mqtt.on_connect = mqtt_connect
+client_mqtt.on_message = mqtt_message
+client_mqtt.connect("localhost", 1883, 60)
+client_mqtt.loop_forever()
