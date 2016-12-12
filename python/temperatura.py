39b6e2447ce38a6bf25247943e0321b7b02101fe
Andrei Bosco <andreibosco@gmail.com>
2016-12-12 15:13:58 -0300

    python: melhorando leitura de temperatura

--- python/temperatura.py
+++ python/temperatura.py
@@ -1,8 +1,30 @@
+import logging
 import w1thermsensor
+import datetime
+from pymongo import MongoClient
 from w1thermsensor import W1ThermSensor
 
 
-def temperatura():
-  sensor = W1ThermSensor()
-  temp_celsius = sensor.get_temperature()
-  return
+# Logging config
+logging.basicConfig(filename="/home/pi/logs/temperature_sensor.log",
+                    level=logging.DEBUG,
+                    format="%(asctime)s %(message)s")
+logging.info("====================")
+
+
+# DB
+clientMongo = MongoClient('localhost:27017')
+db = clientMongo.SmartGreen
+
+
+# Temperature Sensor
+sensor = W1ThermSensor()
+temp_celsius = sensor.get_temperature()
+
+
+# Save data
+logging.info(temp_celsius)
+db.teste06.insert({
+    "when": datetime.datetime.utcnow(),
+    "temperature": temp_celsius
+})
