9cd4343aa0d4d647bf1be79c22e0ba8b53daeeb8
Andrei Bosco <andreibosco@gmail.com>
2016-12-11 21:14:02 -0300

    testes: esboco de conexao tcp usando python

--- testes/python/old/gsm-tcp.py
+++ testes/python/old/gsm-tcp.py
@@ -4,6 +4,11 @@ ser = serial.Serial()
 ser.baudrate = 19200
 ser.port = "/dev/ttyAMA0"
 
+def power_on_sim900:
+    ser.write(b'AT\r')
+    ser.write(b'AT+CSCLK=0\r')
+    ser.write(b'AT+CFUN=1\r')
+    # FIXME: continuar
 
 ser.open()
 ser.write(b'AT+CMGF=1\r')
