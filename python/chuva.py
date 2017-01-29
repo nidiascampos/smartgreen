084eda96363bc99a1f18703b21afebc07b0dc335
Andrei Bosco Bezerra Torres <andreibosco@gmail.com>
2017-01-29 00:55:00 +0000

    python: atualizando coleta de dados do sensor de chuva, agora armazenando no mongoDB

--- python/chuva.py
+++ python/chuva.py
@@ -1,11 +1,39 @@
-import os
-import RPi.GPIO as GPIO
+import logging
+import datetime
+import CHIP_IO.GPIO as GPIO
+from pymongo import MongoClient
+from os.path import expanduser
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN)
-state = GPIO.input(18)
+home = expanduser("~")
 
-if (state == 0):
-    print("Chuva")
-else:
-    print("Sem chuva")
+# Logging config
+logging.basicConfig(filename=home+"/logs/rain_sensor.log",
+                    level=logging.DEBUG,
+                    format="%(asctime)s %(message)s")
+logging.info("====================")
+
+
+# DB
+clientMongo = MongoClient('localhost:27017')
+db = clientMongo.SmartGreen
+
+
+# Pin configuration.
+pin = "XIO-P0"
+
+
+# Rain Sensor
+GPIO.setup(pin, GPIO.IN)
+sensor_read = GPIO.input(pin)
+
+
+# Save data
+logging.info(sensor_read)
+db.coleta02.insert({
+    "when": datetime.datetime.utcnow(),
+    "rain": sensor_read
+})
+
+
+# Cleanup GPIO
+GPIO.cleanup("XIO-P0")
