ee71227b5b0f55dd7e54de38cd4e9dafd5f4d8ee
Andrei Bosco Bezerra Torres <andreibosco@gmail.com>
2017-01-30 14:45:43 -0300

    python: adicionando script que salva leitura de dados do C.H.I.P (temperatura interna, voltagem da bateria e corrente da bateria) no mongoDB

---
+++ python/bateria.py
@@ -0,0 +1,35 @@
+import logging
+import datetime
+import subprocess
+from pymongo import MongoClient
+
+# Logging config
+logging.basicConfig(filename="/var/log/smartgreen/rain_sensor.log",
+                    level=logging.DEBUG,
+                    format="%(asctime)s %(message)s")
+#logging.info("====================")
+
+
+# DB
+clientMongo = MongoClient('localhost:27017')
+db = clientMongo.SmartGreen
+
+
+# Check temperature and battery status (bash script)
+# this script must be on root's $PATH
+# the script output should be:
+# temperature,battery voltage,battery current
+# (celsius, miliVolts, miliAmp)
+chip_status = subprocess.check_output(['battery-vbus.sh'])
+chip_status = battery_status.strip().split(',')
+
+
+# Save data
+logging.info(sensor_read)
+db.coleta02.insert({
+    "when": datetime.datetime.utcnow(),
+    "temperature_internal": chip_status[0],
+    "battery_voltage": chip_status[1],
+    "battery_current": chip_status[2]
+})
+
