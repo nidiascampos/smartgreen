e5b02b00391799f18792cb175e19374e944486a7
Andrei Bosco Bezerra Torres <andreibosco@gmail.com>
2017-02-28 11:51:07 -0300

    matlab kalman: diversos testes com kalman e wrKF

--- matlab/testes_wrKF.m
+++ matlab/testes_wrKF.m
@@ -1,12 +1,24 @@
-load Qoff.data;
-Yn = Qoff(:,4);
+% load Qoff.data;
+% Yn = Qoff(:,4);
+%
+% utilizando dados gerados por 'mlog_2017_coleta_02_c.mlx'
+
+% gerando transpostas para facilitar o uso do filtro
+% sensor1meanT = sensor1meanOrig';
+Yn = [sensor1meanOrig(:,1); sensor1meanOrig(:,2)];
+
+% 1 sensor
+% sensor1fusedKAF1 = kalman([sensor1meanT(1,:)], A, C1, R1, Q);
+% 2 sensor
+% sensor1fusedKAF2 = kalman([sensor1meanT(1,:); sensor1meanT(2,:)], A, [C1; C2], [R1 0; 0 R2], Q);
+
 x = 1; % initial state value
 P = 0.01;
 A = 1;
-C = 1;
+C = [1; 1];
 Q = .005;
 % R = 1e-4;
-R = 0.64;
+R = [0.64 0; 0 0.64];
 ss_wrKF.sum_wzxT = 0;                
 ss_wrKF.sum_wxxT = 0;
 ss_wrKF.sum_xxold = 0;
