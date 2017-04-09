c054b196e26a70a0b4f2916369ceafe0d9d8646e
Andrei Bosco Bezerra Torres <andreibosco@gmail.com>
2017-04-08 22:39:09 -0300

    [testes: rf24comm] - substituindo código pelo exemplo do helloworld_tx do RF24Network (mais compacto e menos propenso a falhas); - adicionando lib RF24Network às dependências do projeto;

--- testes/RF24comm/src/main.cpp
+++ testes/RF24comm/src/main.cpp
@@ -1,94 +1,84 @@
+/*
+ Copyright (C) 2012 James Coliz, Jr. <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+
+ Update 2014 - TMRh20
+ Update 2016 - Andrei Bosco
+ */
+
 #include <Arduino.h>
+#include <RF24Network.h>
+#include <RF24.h>
 #include <SPI.h>
-#include <printf.h>
-#include "RF24.h"
 
-// Set this radio as radio number 0 or 1
-bool radioNumber = 0;
+RF24 radio(2,10);                    // nRF24L01(+) radio attached using Getting Started board
 
-// Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 2 & 10
-RF24 radio(2,10);
+RF24Network network(radio);          // Network uses that radio
 
-// Nodes addresses
-byte addresses[][6] = {"1Node","2Node"};
+const uint16_t this_node = 02;       // Address of our node in Octal format
+const uint16_t other_node = 00;      // Address of the other node in Octal format
 
-void setup() {
-  // Open Serial Communication
-  Serial.begin(115200);
-  Serial.println(F("TESTE DE COMUNICACAO"));
+const unsigned long interval = 2000; //ms  // How often to send 'hello world to the other unit
 
-  // Radio Settings
+unsigned long last_sent;             // When did we last send?
+unsigned long packets_sent;          // How many have we sent already
+
+
+struct payload_t {                  // Structure of our payload
+  unsigned long wm15;
+  unsigned long wm45;
+  unsigned long wm75;
+  unsigned long counter;
+};
+
+void setup(void)
+{
+  Serial.begin(57600);
+  Serial.println("RF24Network Test");
+
+  SPI.begin();
   radio.begin();
-  radio.setChannel(100); // Communication Channel: 0 - 127
-  radio.setCRCLength(RF24_CRC_8); // CRC: 1 = 8 bits, 2 = 16 bits
-  radio.setPALevel(RF24_PA_MAX); // RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH and RF24_PA_MAX
-  radio.setDataRate(RF24_1MBPS); // 0 = RF24_1MBPS for 1Mbps, 1 = RF24_2MBPS for 2Mbps, 2 = RF24_250KBPS for 250kbs
-  radio.setAutoAck(1); // ACK: 1 = Enable, 0 = Disable
-  radio.setRetries(15,15); // Set the number and delay of retries upon failed submit
-
-  // Print radio settings.
-  printf_begin();
-  radio.printDetails();
-
-  // Open a writing and reading pipe on each radio, with opposite addresses
-  if(radioNumber){
-    radio.openWritingPipe(addresses[1]);
-    radio.openReadingPipe(1,addresses[0]);
-  }else{
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1,addresses[1]);
-  }
-
-  // Start the radio listening for data
-  radio.startListening();
+  // Format: channel, node address
+  network.begin(90, this_node);
 }
 
 void loop() {
 
-  radio.stopListening();                                    // First, stop listening so we can talk.
+  network.update();                          // Check the network regularly
 
-  unsigned long start_time = micros();                      // Take the time, and send it.  This will block until complete
-  Serial.print(F("Sending payload: "));
-  Serial.println(start_time);
-  Serial.print(F("Payload size: "));
-  Serial.println(sizeof(unsigned long));
-   if (!radio.write( &start_time, sizeof(unsigned long) )){
-     Serial.println(F("Failed to send"));
-   }
+  unsigned long now = millis();              // If it's time to send a message, send it!
+  if ( now - last_sent >= interval  )
+  {
+    last_sent = now;
 
-  // radio.startListening();                                    // Now, continue listening
-  //
-  // unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-  // boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-  //
-  // while ( ! radio.available() ){                             // While nothing is received
-  //   if (micros() - started_waiting_at > 400000 ){            // If waited longer than 400ms, indicate timeout and exit while loop
-  //       timeout = true;
-  //       break;
-  //   }
-  // }
-  //
-  // if ( timeout ){                                             // Describe the results
-  //     Serial.println(F("Response failed, timed out."));
-  //     Serial.println();
-  // }else{
-  //     unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
-  //     radio.read( &got_time, sizeof(unsigned long) );
-  //     unsigned long end_time = micros();
-  //
-  //     // Spew it
-  //     Serial.print(F("Sent "));
-  //     Serial.print(start_time);
-  //     Serial.print(F(", Got response "));
-  //     Serial.print(got_time);
-  //     Serial.print(F(", Round-trip delay "));
-  //     Serial.print(end_time-start_time);
-  //     Serial.println(F(" microseconds"));
-  //     Serial.println();
-  // }
+    Serial.println("Sending...");
 
-  // Try again 5s later
-  delay(5000);
+    int wm15data = 1300;
+    int wm45data = 3500;
+    int wm75data = 7000;
 
+    // char outputBuf[10];
+    // message.toCharArray(outputBuf, 10);
 
-} // Loop
+    payload_t payload = { wm15data, wm45data, wm75data, packets_sent++ };
+
+    Serial.print("Payload id: ");
+    Serial.print(payload.counter);
+    Serial.print(" | wm15: ");
+    Serial.print(payload.wm15);
+    Serial.print(" | wm45: ");
+    Serial.print(payload.wm45);
+    Serial.print(" | wm75: ");
+    Serial.println(payload.wm75);
+
+    RF24NetworkHeader header(/*to node*/ other_node);
+    bool ok = network.write(header,&payload,sizeof(payload));
+    if (ok)
+      Serial.println("ok.");
+    else
+      Serial.println("failed.");
+  }
+}
