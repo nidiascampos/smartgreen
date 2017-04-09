503342f1c06e9b49291422ff6ecd0610bc6ef3c9
Andrei Bosco Bezerra Torres <andreibosco@gmail.com>
2017-04-09 12:29:37 -0300

    [testes: rf24comm] - adicionando sleep; - removendo código desnecessário; - mensagens agora são enviadas sempre que o arduino acordar do sleep;

--- testes/RF24comm/src/main.cpp
+++ testes/RF24comm/src/main.cpp
@@ -13,19 +13,18 @@
 #include <RF24Network.h>
 #include <RF24.h>
 #include <SPI.h>
+#include "Sleep.h"
+
+void sleepModule();
 
 RF24 radio(2,10);                    // nRF24L01(+) radio attached using Getting Started board
 
 RF24Network network(radio);          // Network uses that radio
 
-const uint16_t this_node = 02;       // Address of our node in Octal format
+const uint16_t this_node = 01;       // Address of our node in Octal format
 const uint16_t other_node = 00;      // Address of the other node in Octal format
 
-const unsigned long interval = 2000; //ms  // How often to send 'hello world to the other unit
-
-unsigned long last_sent;             // When did we last send?
 unsigned long packets_sent;          // How many have we sent already
-
 
 struct payload_t {                  // Structure of our payload
   unsigned long wm15;
@@ -39,6 +38,14 @@ void setup(void)
   Serial.begin(57600);
   Serial.println("RF24Network Test");
 
+  //------------ SLEEP ------------
+  // set sleep time in ms, max sleep time is 49.7 days
+  // sleepTime = 60000; // 1 minuto
+  // sleepTime = 300000; // 5 minutos
+  // sleepTime = 900000; // 15 minutos
+  // sleepTime = 1800000; // 30 minutos (1000 * 60 * 30)
+  sleepTime = 3600000; // 1 hora
+
   SPI.begin();
   radio.begin();
   // Format: channel, node address
@@ -47,38 +54,54 @@ void setup(void)
 
 void loop() {
 
+  radio.powerUp();
+  delay(100);
+
   network.update();                          // Check the network regularly
 
-  unsigned long now = millis();              // If it's time to send a message, send it!
-  if ( now - last_sent >= interval  )
-  {
-    last_sent = now;
+  Serial.println("Sending...");
 
-    Serial.println("Sending...");
+  int wm15data = 1000;
+  int wm45data = 3000;
+  int wm75data = 5000;
 
-    int wm15data = 1300;
-    int wm45data = 3500;
-    int wm75data = 7000;
+  // char outputBuf[10];
+  // message.toCharArray(outputBuf, 10);
 
-    // char outputBuf[10];
-    // message.toCharArray(outputBuf, 10);
+  payload_t payload = { wm15data, wm45data, wm75data, packets_sent++ };
 
-    payload_t payload = { wm15data, wm45data, wm75data, packets_sent++ };
+  Serial.print("Payload id: ");
+  Serial.print(payload.counter);
+  Serial.print(" | wm15: ");
+  Serial.print(payload.wm15);
+  Serial.print(" | wm45: ");
+  Serial.print(payload.wm45);
+  Serial.print(" | wm75: ");
+  Serial.println(payload.wm75);
 
-    Serial.print("Payload id: ");
-    Serial.print(payload.counter);
-    Serial.print(" | wm15: ");
-    Serial.print(payload.wm15);
-    Serial.print(" | wm45: ");
-    Serial.print(payload.wm45);
-    Serial.print(" | wm75: ");
-    Serial.println(payload.wm75);
+  RF24NetworkHeader header(/*to node*/ other_node);
+  bool ok = network.write(header,&payload,sizeof(payload));
+  if (ok)
+    Serial.println("ok.");
+  else
+    Serial.println("failed."); // FIXME: tentar fazer com que ele tente reenviar em caso de erro
 
-    RF24NetworkHeader header(/*to node*/ other_node);
-    bool ok = network.write(header,&payload,sizeof(payload));
-    if (ok)
-      Serial.println("ok.");
-    else
-      Serial.println("failed.");
-  }
+  radio.powerDown();
+
+  //---------- SLEEP -----------
+  sleepModule();
+
+}
+
+void sleepModule() {
+  // Enter power down state with ADC and BOD module disabled.
+  // Wake up when wake up pin is low.
+  Serial.print("Sleeping for ");
+  Serial.print(sleepTime / 60000);
+  Serial.println(" minute(s)");
+
+  delay(500); // delay to allow serial to fully print before sleep
+
+  sleep.pwrDownMode(); // set sleep mode
+  sleep.sleepDelay(sleepTime); // sleep for: sleepTime
 }
