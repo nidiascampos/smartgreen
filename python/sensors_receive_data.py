c5ebe36f4da0daa978550c77ee70a295b245f225
Andrei Bosco Bezerra Torres <andreibosco@gmail.com>
2017-04-09 21:22:28 -0300

    [python] - adicionando saída para arquivo de log

--- python/sensors_receive_data.py
+++ python/sensors_receive_data.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 
-#
-# Simplest possible example of using RF24Network,
-#
-#  RECEIVER NODE
-#  Listens for messages from the transmitter and prints them out.
-#
+# Based on RF24Network helloworld_rx.py
+
 from __future__ import print_function
 import time
 import datetime
@@ -17,29 +13,35 @@ from RF24 import *
 from RF24Network import *
 
 # CE Pin, CSN Pin, SPI Speed
-
 radio = RF24(RPI_V2_GPIO_P1_15, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ)
 network = RF24Network(radio)
 
-# millis = lambda: int(round(time.time() * 1000))
-octlit = lambda n:int(n, 8)
-
 # Address of our node in Octal format (01, 021, etc)
+octlit = lambda n:int(n, 8)
 this_node = octlit("00")
 
 # Start radio
 radio.begin()
 time.sleep(0.1)
+
 # Format: channel, node address
 network.begin(90, this_node)    # channel 90
+
 # Print radio config
 radio.printDetails()
 packets_sent = 0
 last_sent = 0
 
+# Logging config
+logging.basicConfig(filename="/var/log/smartgreen/sensors_rx_data.log",
+                    level=logging.DEBUG,
+                    format="%(asctime)s %(message)s")
+logging.info("====================")
+
 # MongoDB
 clientMongo = MongoClient('localhost:27017')
 db = clientMongo.SmartGreen
+
 
 def mongo_add_message(module_id, module_vcc, sensor_15cm, sensor_15cm_bias, sensor_45cm, sensor_45cm_bias, sensor_75cm, sensor_75cm_bias):
     # inserting data into mongodb
@@ -56,11 +58,13 @@ def mongo_add_message(module_id, module_vcc, sensor_15cm, sensor_15cm_bias, sens
         "published": "no"
     })
 
+
+# Loop
 while 1:
     network.update()
     while network.available():
         header, payload = network.read(28)
-        print("payload length ", len(payload))
+        print("Payload length:", len(payload))
         # ms, number = unpack('<LL', bytes(payload))
         wm15, wm15bias, wm45, wm45bias, wm75, wm75bias, vcc = unpack('<llllllf', bytes(payload))
         # print('Received payload ', number, ' at ', ms, ' from ', oct(header.from_node))
