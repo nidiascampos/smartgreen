8faab920a46e6708c865eccd717f1e92efbdd772
Andrei Bosco <andreibosco@virtual.ufc.br>
2017-04-13 15:36:23 -0300

    [python] sensor_rain: - revertendo ao método de pooling; - organizando melhor o código em funções

--- python/sensor_rain.py
+++ python/sensor_rain.py
@@ -15,47 +15,36 @@ clientMongo = MongoClient('localhost:27017')
 db = clientMongo.SmartGreen
 
 
-# Pin configuration.
+# Pin configuration
 GPIO.setmode(GPIO.BCM)
 pin = 26
 
 
-# Rain Sensor
+# Save data to MongoDB
+def mongo_add_message(rain):
+    # inserting data into mongodb
+    db.teste07.insert({
+        "when": datetime.datetime.utcnow(),
+        "rain": rain
+    })
+
+
+# Read Rain Sensor
 GPIO.setup(pin, GPIO.IN)
-GPIO.add_event_detect(pin, GPIO.FALLING)
-# sensor_read = GPIO.input(pin)
-
-# if sensor_read == 0:
-#     rain = True
-# else:
-#     rain = False
-
-while 1:
-    print "Loop iniciado"
-    if GPIO.event_detected(pin):
-        print("Chuva")
-        print("Saindo do loop")
-        logging.info("Chuva")
-        rain = True
-    break
+sensor_read = GPIO.input(pin)
 
 
-# Save data
-# logging.info(sensor_read)
-db.teste07.insert({
-    "when": datetime.datetime.utcnow(),
-    "rain": rain
-})
+# Check if it's raining or not and save to MongoDB
+if sensor_read == 0:
+    rain = True
+    print "Chuva"
+    logging.info("Chuva")
+else:
+    rain = False
+    print "Sem chuva"
+    logging.info("Sem chuva")
+mongo_add_message(rain)
 
-
-time.sleep(10)
 
 # Cleanup GPIO
 GPIO.cleanup(pin)
-
-# Exemplo
-# add rising edge detection on a channel
-# GPIO.add_event_detect(channel, GPIO.RISING)
-# do_something()
-# if GPIO.event_detected(channel):
-#     print('Button pressed')
