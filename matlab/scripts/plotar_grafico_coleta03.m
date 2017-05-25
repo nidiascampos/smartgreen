f8bf2a9dec51de0a4cc380e43c1fb0ffe57369ff
Andrei Bosco Bezerra Torres <andreibosco@gmail.com>
2017-05-25 09:56:15 -0300

    [matlab] corrigindo nome de função e removendo codigo desnecessario

--- matlab/scripts/plotar_grafico_coleta03.m
+++ matlab/scripts/plotar_grafico_coleta03.m
@@ -1,18 +1,15 @@
 %% funcao de plotar e gravar
-function plotar_grafico(periodo,sensor,nome,titulo,labelY,exibirPlot)
+function plotar_grafico_coleta03(periodo,sensor,nome,titulo,labelY,exibirPlot)
     grafico = figure('visible',exibirPlot);
     hold on
     grid on
     plot(periodo,sensor,'*-');
-%     for i = 1:size(outlierIndex,1)
-%         plot(periodo(outlierIndex(i,1)),outlierIndex(i,2),'LineStyle','none','Marker','o','MarkerEdgeColor','red')
-%     end
     hold off
-%     titleOutliers = sprintf('Total de outliers removidos: %d',outlierCount);
     methodName = sprintf('%s',titulo);
     title(methodName);
     xlabel('Período (dias)');
     ylabel(labelY);
+%     ylim([-200 7000]);
 %     legend('nó 1','nó 2','nó 3','nó 4','fusão','outlier','location',posicaoLegenda)
     filename = sprintf('graphs/coleta03/%s',nome);
     saveas(grafico,filename,'png');
