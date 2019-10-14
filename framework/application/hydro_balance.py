7b76cbf0d226d5a44366568f6f2c9714e3c980e9
Nídia Campos <nidiascampos@gmail.com>
2019-10-14 12:59:34 -0300

    Add files via upload

--- framework/application/hydro_balance.py
+++ framework/application/hydro_balance.py
@@ -8,11 +8,14 @@ from service import Storage as stor
 
 from application.pyeto import fao
 
+from application.soil_moisture_prediction import MoisturePrediction
 
 class HydroBalance:
     def __init__(self,db_host,db_user,db_pass):
         self.database = stor.Storage(db_host, db_user, db_pass)
         self.fusion = fus.Fusion()
+        self.moist_pred=MoisturePrediction()
+        self.moist_pred_type=0
 
         self.field_id=0
         self.farm_id=0
@@ -38,6 +41,9 @@ class HydroBalance:
         self.monitoring_points_ids=[]
         self.soil_layer_data={}
         self.soil_moisture = 0
+        self.soil_mois_pred=0
+        self.fusion_data={}
+        self.soil_moisture_data={}
 
         #weather station info
         self.weather_station_id=0
@@ -107,8 +113,7 @@ class HydroBalance:
                 layer_depth_type = layer[1]
                 moisture_sensor_type_id = layer[4]
 
-                u = self.database.get_moisture_data(self.date, self.field_id, mp_id,
-                                                    soil_layer_id)
+                u = self.database.get_moisture_data(self.date, self.field_id, mp_id,soil_layer_id)
                 criterias = self.database.get_moisture_remotion_criteria(moisture_sensor_type_id)
                 self.fusion.set_soil_moisture_remotion_criteria(criterias["min"],
                                                                 criterias["max"])
@@ -120,17 +125,6 @@ class HydroBalance:
 
                 data.append(u)
                 self.soil_layer_data.update({layer_depth_type:data})
-
-    # model to compute soil moisture
-    def vanGenuchten(self, thetaR, thetaS, alpha, n, psiM):
-        num = thetaS - thetaR
-
-        den = (alpha * psiM) ** n
-
-        den = (den + 1) ** (1 - 1 / n)
-
-        theta = thetaR + (num / den)
-        return theta
 
     def compute_soil_moisture(self,date):
         self.data_preprocessing(date)
@@ -144,6 +138,7 @@ class HydroBalance:
         alpha_air_entry_suction = 0
         n_pore_size_distribution = 0
         fusion_id=0
+        self.soil_moisture_data={}
 
         for key in self.soil_layer_data.keys():
             layer=self.database.get_soil_depth(key)
@@ -166,13 +161,94 @@ class HydroBalance:
 
             # a=current_humidity*layer_depth
             # print("layer_depth {}".format(a))
-            self.soil_moisture+=current_humidity*layer_depth
+            u=current_humidity*layer_depth
+            self.fusion_data.update({layer_depth:fusioned_data})
+            self.soil_moisture_data.update({layer_depth:u})
 
-    #return water need in mm
-    def compute_RNI(self,start_date,stop_date,field_id,station_id,with_soil_data):
+        z=min(self.soil_moisture_data.keys())
+        self.soil_moisture=self.soil_moisture_data[z]
+
+    def run_local_prediction(self,date,fusion_type="zscore"):
+        monitoring_points_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        predict_data=[]
+        p = 9
+        management_type = 4
+        W = 0
+        res = 0
+        soil_layers = []
+        self.soil_layer_data = {}
+        data=0
+
+        #get W
+        for mp_id in monitoring_points_ids:
+            res = self.database.get_irrigation_water(self.farm_id, self.field_id, mp_id, management_type, date)
+            if res != None:
+                W += res
+            else:
+                p = p - 1
+
+        if p > 0:
+            W = W / p
+
+        #forecast soil moisture
+        for mp_id in monitoring_points_ids:
+            data = self.moist_pred.local_prediction(mp_id, self.kc,
+                                                    self.weather_data["T_max"],
+                                                    self.weather_data["RH_min"],
+                                                    self.weather_data["RH_max"],
+                                                    self.weather_data["Ri"],
+                                                    W, self.weather_data["Rn"],
+                                                    self.weather_data["U"],
+                                                    self.weather_data["T_min"],
+                                                    self.weather_data["P"])
+            predict_data.append(data)
+
+        #fusion data
+        fusioned_data = self.fusion.apply_method(fusion_type,predict_data)
+
+        self.soil_mois_pred = self.vanGenuchten(0.14010, 0.38839, 0.022504, 20.524, fusioned_data)
+        self.soil_mois_pred = self.soil_mois_pred * 150
+        # print("date {} W {} pm {} moist {}".format(date, W, pm, self.soil_mois_pred))
+        return self.soil_mois_pred
+
+    def run_global_prediction(self,date):
+        monitoring_points_ids=[1,2,3,4,5,6,7,8,9]
+        p=9
+        management_type=4
+        W=0
+        res=0
+        for id in monitoring_points_ids:
+            res=self.database.get_irrigation_water(self.farm_id,self.field_id,id,management_type,date)
+            if res!=None:
+                W+=res
+            else:
+                p=p-1
+
+        if p>0:
+            W=W/p
+
+        pm=self.moist_pred.global_prediction(self.kc,
+                                          self.weather_data["T_max"],
+                                          self.weather_data["RH_min"],
+                                          self.weather_data["RH_max"],
+                                          self.weather_data["Ri"],
+                                          W,self.weather_data["Rn"],
+                                          self.weather_data["U"],
+                                          self.weather_data["T_min"],
+                                          self.weather_data["P"])
+
+        self.soil_mois_pred=self.vanGenuchten(0.14010,0.38839,0.022504,20.524,pm)
+        self.soil_mois_pred=self.soil_mois_pred*150
+        #print("date {} W {} pm {} moist {}".format(date, W, pm, self.soil_mois_pred))
+        return self.soil_mois_pred
+
+    #return water need in mm; prediction= 0 (without),1(local approach), 2 (global)
+    def compute_RNI(self,start_date,stop_date,field_id,station_id,with_soil_data, prediction_type):
         self.set_field_info(field_id)
         self.set_crop_info()
         self.set_irrigation_system_info()
+        self.moist_pred_type=prediction_type
+
 
         d1 = start_date.split("-")
         d2 = stop_date.split("-")
@@ -191,8 +267,9 @@ class HydroBalance:
         for day in range(0, num_days):
             self.weather_data= self.database.get_meteorological_data(station_id, start_date)
             nhb=self.execute_hydro_balance(stage)
+
             self.NHB+=nhb
-            print("date {} stage {} nhb_day {} NHB {}".format(start_date, stage, nhb,self.NHB))
+            #print("date {} stage {} nhb_day {} NHB {}".format(start_date, stage, nhb,self.NHB))
             day1 = day1 + timedelta(days=1)
             start_date = day1.strftime("%Y-%m-%d")
             stage += 1
@@ -201,6 +278,21 @@ class HydroBalance:
         if with_soil_data==True and len(self.monitoring_points_ids) > 0:
             self.compute_soil_moisture(stop_date)
             self.NHB-=self.soil_moisture
+
+        if self.moist_pred_type == 1 and nhb != 0:
+            day1 = day1 - timedelta(days=1)
+            start_date = day1.strftime("%Y-%m-%d")
+            stage -= 1
+            self.run_global_prediction(start_date)
+            self.NHB -= self.soil_mois_pred
+
+        elif self.moist_pred_type == 2 and nhb != 0:
+            day1 = day1 - timedelta(days=1)
+            start_date = day1.strftime("%Y-%m-%d")
+            stage -= 1
+            self.run_local_prediction(start_date)
+
+            self.NHB -= self.soil_mois_pred
 
         self.ITN=self.NHB/self.irrigation_ef
 
@@ -246,9 +338,11 @@ class HydroBalance:
                              self.weather_data["Rn"],
                              self.weather_data["U"],
                              self.weather_data["P"])
+
             self.compute_current_kc(stage)
             self.ETc = self.kc * self.ETo
             nhb=self.ETc - self.weather_data["Ri"]
+
 
         return nhb
 
@@ -261,14 +355,48 @@ if __name__ == '__main__':
     station_id = 1
     field_id = 1
     farm_id=1
-    start_date = "2016-07-1"
-    stop_date = "2016-07-30"
+    start_date = "2016-5-23"
+    stop_date = "2016-5-27"
+    moist_pred_type=2# 0 para não usadar dados da predição 1 para predição global e 2 para predição local de dados do
+    water_needed = []
+    moist_pred = []
+    NHB=[]
+    w = 0
+    fusion_type="zscore"
 
-    water_needed=irrigation_management.compute_RNI(start_date,stop_date,
-                                                   field_id,station_id,False)
+    soil_moist_data=False # usar dados do solo no balanço
 
+    #Balanço Hidrico para um periodo
+    w = irrigation_management.compute_RNI(start_date, stop_date, field_id, station_id,
+                                          soil_moist_data, moist_pred_type)
+    if w != 0:
+        water_needed.append(w)
+        moist_pred.append(irrigation_management.soil_mois_pred)
+        NHB.append(irrigation_management.NHB)
+
+    print("soil_moisture {}".format(moist_pred))
+    print("NHB {}".format(NHB))
     print("water_needed {}".format(water_needed))
-    irrigation_management.compute_irrigation_time()
+
+    # #Balanço Hidrico Diário dado um periodo
+    # d1 = start_date.split("-")
+    # d2 = stop_date.split("-")
+    # day1 = date(int(d1[0]), int(d1[1]), int(d1[2]))
+    # day2 = date(int(d2[0]), int(d2[1]), int(d2[2]))
+    # num_days = (day2 - day1).days
+    #
+    # for day in range(0, num_days):
+    #     w = irrigation_management.compute_RNI(start_date, start_date, field_id, station_id, False,moist_pred_type)
+    #     if w != 0:
+    #         water_needed.append(w)
+    #         moist_pred.append(irrigation_management.soil_mois_pred)
+    #         NHB.append(irrigation_management.NHB)
+    #     day1 = day1 + timedelta(days=1)
+    #     start_date = day1.strftime("%Y-%m-%d")
+    #
+    # print("soil_moisture {}".format(moist_pred))
+    # print("NHB {}".format(NHB))
+    # print("water_needed {}".format(water_needed))
 
     # database = stor.Storage('localhost', 'root', '12345678')
     # monitoring_point_id=0#if the field does not have monitoring points
